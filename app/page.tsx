09fc48cbd033143fe6204e91c6ca93649f4a4da6
anna-gubler <agubler@quickline.ch>
2026-03-18 15:45:33 +0100

    Test: debug sonarcloud once more

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import { useRouter } from "next/navigation";
-import Image from "next/image";
+// import Image from "next/image";
 import { Button } from "antd";
-import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
+// import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
 import styles from "@/styles/page.module.css";
 
 export default function Home() {
